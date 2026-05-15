bae0d03dcbc591bf6a2c1cf597f4e5d649fd2487
Sunanda Samaddar <44464454+SunandaSamaddar@users.noreply.github.com>
2026-05-15 10:48:36 +0000

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # barsoom
+
+# Hello world JavaScript action
+
+This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+
+## Inputs
+
+### `who-to-greet`
+
+**Required** The name of the person to greet. Default `"World"`.
+
+## Outputs
+
+### `time`
+
+The time we greeted you.
+
+## Example usage
+
+```yaml
+uses: actions/barsoom@f101851882d4dce6a23fcb192c88bfc698d85591
+with:
+  name: Mona the Octocat
+```
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
-const name = core.getInput('name');
 
-// console.log(`Hello ${name} from Barsoom!`);
-const outputValue = `Hello ${name}!`;
-core.setOutput('greeting', outputValue);
+try {
+
+    const name = core.getInput('name');
+
+    // console.log(`Hello ${name} from Barsoom!`);
+    const outputValue = `Hello ${name}!`;
+    core.setOutput('greeting', outputValue);
+} catch (error) {
+    core.setFailed(error.message);
+}
